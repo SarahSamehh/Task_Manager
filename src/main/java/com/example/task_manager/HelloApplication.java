ee3027ac87540c23defd5e6f279687d4c24d47d8
SarahSamehh <sarahsameh7@gmail.com>
2024-05-13 14:49:04 +0300

    /Confirm Exit/

--- src/main/java/com/example/task_manager/HelloApplication.java
+++ src/main/java/com/example/task_manager/HelloApplication.java
@@ -1,8 +1,11 @@
 package com.example.task_manager;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -16,6 +19,19 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         NotificationThread.startNotifications(10, TimeUnit.SECONDS);
+        stage.setOnCloseRequest(event -> {
+            event.consume(); // Consume event so the window doesn't close immediately
+            confirmExit(stage);
+        });
+    }
+    private void confirmExit(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Exit");
+        alert.setHeaderText("Thank you for using Task Manager! Are you sure you want to exit?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) { //On user confirmation
+            Platform.exit(); // Close Application
+        }
     }
 
     public static void main(String[] args) {
